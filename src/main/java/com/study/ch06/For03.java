045248373158c855696fad6638b45fd155b6b638
dpghdfuf <dpghdfuf@hanmail.net>
2024-01-04 09:12:37 +0900

    test

--- src/main/java/com/study/ch06/For03.java
+++ src/main/java/com/study/ch06/For03.java
@@ -21,20 +21,129 @@ public class For03 {
 *
 **
 ***
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i<=N; i++) {
+            for (int j = 1; j<=i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
+
+
 
 ***
 **
 *
 
+#include<stdio.h>
+int main()
+{
+    int N,i,k;
+    scanf("%d", &N);
+    for(i=1; i<=N; i++)
+    {
+        for(k=1; k<=N+1-i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+
+
   *
  **
 ***
 
+        import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 1; i<= N; i++) {
+            for(int j=N-1; j>=i; j-- ) {
+                System.out.print(" ");
+            }
+            for(int k=1; k<=i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+}
 ***
  **
   *
 
+        #include<stdio.h>
+int main()
+{
+    int N,i,k,l,space;
+    scanf("%d", &N);
+    for(i=1; i<=N; i++)
+    {
+        if(i>1)
+        {
+            for(l=1; l<i; l++)
+            {
+                printf(" ");
+            }
+        }
+        for(k=1; k<=N+1-i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
   *
  ***
 *****
-//숫자 거꾸로가는거랑
+
+          import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i<=N; i++) {
+            for(int j = N-1; j>=i; j--) {
+                System.out.print(" ");
+            }
+            for(int k =1; k<=2*i-1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+
+
+    }
+}
+
